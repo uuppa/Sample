48a21b0bbbb6a7797a9e61bf557c709491f6cc0c
uuppa <uuppa@allstate.com>
2016-05-04 09:22:23 -0500

    Update CalculatorTest.java

--- src/com/junit/test/CalculatorTest.java
+++ src/com/junit/test/CalculatorTest.java
@@ -1,13 +1,18 @@
 package com.junit.test;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest {
   @Test
   public void evaluatesExpression() {
+    BasicConfigurator.configure();
     Calculator calculator = new Calculator();
     int sum = calculator.evaluate("1+2+3");
     assertEquals(6, sum);
+    logger.info("CalculatorTest"); 
   }
 }
